05a1d7d4ac6b21c76633ed6c604aaff741c714b3
John Macharia <machariajohn23@gmail.com>
2023-04-19 17:39:33 +0300

    1st step of the console interpretor

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This file contains the entry point of the Buy_furni command interpreter"""
+
+
+import cmd
+
+
+class Buy_furni_Command(cmd.Cmd):
+    """simple framework for writing line-oriented Hbnb command interpreter"""
+    prompt = "(Buy_Furni) "
+
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, args):
+        """Exits from the console"""
+        return True
+
+    def do_EOF(self, line):
+        """Gives a clean way to exit interpretor"""
+        return True
+
+
+if __name__ == '__main__':
+    Buy_furni_Command().cmdloop()
