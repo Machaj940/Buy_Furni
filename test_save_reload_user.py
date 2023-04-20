d004335ca7bed769886b70c568f694eded33a4a6
John Macharia <machariajohn23@gmail.com>
2023-04-20 22:05:50 +0300

    Test if File Storage picks up all the classes in models

--- test_save_reload_user.py
+++ test_save_reload_user.py
@@ -3,6 +3,11 @@ from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.client import Client
+from models.county import County
+from models.furniture_type import FurnitureType
+from models.product import Product
+from models.review import Review
+from models.sub_county import SubCounty
 
 all_objs = storage.all()
 print("-- Reloaded objects --")
@@ -10,7 +15,7 @@ for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
     print(obj)
 
-print("-- Create a new User --")
+print("-- Create a new Client --")
 my_client = Client()
 my_client.full_name = "Betty"
 my_client.business_name = "GLS"
@@ -26,3 +31,34 @@ my_user2.email = "airbnb2@mail.com"
 my_user2.password = "root"
 my_user2.save()
 print(my_user2)
+
+print("-- Create a county --")
+my_county = County()
+my_county.name = "Nairobi"
+my_county.save()
+print(my_county)
+
+print("-- Create a furniture type --")
+my_furni_type = FurnitureType()
+my_furni_type.name = "Beds"
+my_furni_type.save()
+print(my_furni_type)
+
+print("-- Create a product --")
+my_product = Product()
+my_product.name = "L-seat"
+my_product.price = 500
+my_product.save()
+print(my_product)
+
+print("-- Create a review --")
+my_review = Review()
+my_review.text = "Very comfortable"
+my_review.save()
+print(my_review)
+
+print("-- Create a sub-county --")
+my_sub_county = SubCounty()
+my_sub_county.name = "Dagoretti"
+my_sub_county.save()
+print(my_sub_county)
