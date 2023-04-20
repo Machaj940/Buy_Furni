5b992bd5fb09cb0d9889048baf1f4e11f859dd47
John Macharia <machariajohn23@gmail.com>
2023-04-20 19:07:41 +0300

    The User class represents the sellers that subscribe to Buy_furni

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Class User inherits from BaseModel"""
+"""Users are the sellers that list their products on Buy_Furni"""
 
 
 from models.base_model import BaseModel
@@ -7,7 +7,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """the User class"""
+    Business_name = ""
     email = ""
     password = ""
-    first_name = ""
-    last_name = ""
+    phone_number = ""
