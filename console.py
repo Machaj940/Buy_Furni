b23484748c6567c2fc689a5ba7d3ed0e3cf25aae
John Macharia <machariajohn23@gmail.com>
2023-04-20 22:06:55 +0300

    Update command interpreter to allow show, create, destroy, update and all to be used by all classes in models

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.client import Client
+from models.county import County
+from models.furniture_type import FurnitureType
+from models.product import Product
+from models.review import Review
+from models.sub_county import SubCounty
 
 
 class Buy_furni_Command(cmd.Cmd):
