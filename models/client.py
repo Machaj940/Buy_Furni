5053d7cf5b3ada263063a2be2bf5724c55325efa
John Macharia <machariajohn23@gmail.com>
2023-04-24 19:24:42 +0300

    Need to deal with class attributes that take 2 or more names as values as explained in the class docstring

--- models/client.py
+++ models/client.py
@@ -6,8 +6,15 @@ from models.base_model import BaseModel
 
 
 class Client(BaseModel):
-    """the User class"""
-    full_name = ""
+    """
+        the User class
+        - will add business_name attribute later. the console currently only
+          takes one argument for certain functions eg Client.update(
+          "<client id>, "business_name" "future Designs") will only
+          register the name as future
+    """
+    first_name = ""
+    last_name = ""
     business_name = ""
     email = ""
     password = ""
--- models/user.py
+++ models/user.py
@@ -6,8 +6,14 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """the User class"""
-    Business_name = ""
+    """
+        the User class
+         - will add business_name attribute later. the console currently only
+          takes one argument for certain functions eg Client.update(
+          "<client id>, "business_name" "future Designs") will only
+          register the name as future
+    """
+    #Business_name = ""
     email = ""
     password = ""
     phone_number = ""
