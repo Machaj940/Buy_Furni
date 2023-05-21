4f88f06e42176330d575d8c6a3fabf24d5a5e73c
John Macharia <machariajohn23@gmail.com>
2023-05-21 23:31:55 +0300

    Make changes to BaseModel initialization

--- models/base_model.py
+++ models/base_model.py
@@ -11,21 +11,19 @@ class BaseModel:
     """BaseModel class for our Airbnb project"""
     def __init__(self, *args, **kwargs):
         """BaseModel initialization with args and kwargs"""
-        if kwargs:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
-        else:
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
+	else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+		if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """How BaseModel shoul be printed"""
